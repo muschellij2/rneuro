9694ed6400271073bd29b08f181968e546f939df
muschellij2 <muschellij2@gmail.com>
2017-03-16 18:44:47 -0400

    updated rconf

--- rneuro_ms_rstudio/userconf.sh
+++ rneuro_ms_rstudio/userconf.sh
@@ -3,7 +3,7 @@
 ## Set defaults for environmental variables in case they are undefined
 USER=${USER:=docker}
 PASSWORD=${PASSWORD:=docker}
-USERID=${USERID:=1000}
+USERID=${USERID:=1001}
 GROUPID=${GROUPID:=1000}
 ROOT=${ROOT:=FALSE}
 UMASK=${UMASK:=022}
